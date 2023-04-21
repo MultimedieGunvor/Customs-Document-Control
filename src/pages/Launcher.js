eaf3b2bca2756ce423e187a205a3504d28aad620
MultimedieGunvor <eaagupa@students.eaaa.dk>
2023-04-21 15:08:17 +0200

    Fetching + storing data

--- src/pages/Launcher.js
+++ src/pages/Launcher.js
@@ -1,20 +1,43 @@
 import { collection, getDocs } from "firebase/firestore";
+// import { useState } from "react";
+
 import { db } from "../firebaseConfig";
 
 export default function Launcher() {
 
-    async function getmanifests() {
+    // const [Manifests, SetManifests] = useState([]);
+    const manifests= [];
+    async function getManifests() {
         const querySnapshot = await getDocs(collection(db, "manifests"));
-        querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data());
+        querySnapshot.forEach((manifest) => {
+            let item = [];
+            item.push(manifest.id);
+            item.push(manifest.data());
+            manifests.push(item);            
         })
+        console.log("manifests: ", manifests);
+        return manifests;
     };
-
-    getmanifests();
+    getManifests();
+    console.log("Testing ", manifests);
 
     return (
         <div>
-            Insert launcher here
+            <div className="filter-box">
+                <legend>Show range</legend>
+                <select className="select-range">
+                    <option value="month">Month</option>
+                </select>
+                <select className="select-type">
+                    <option value="all">All</option>
+                </select>
+                <select className="select-termID">
+                    <option value="all">All</option>
+                </select>
+                <select className="select-status">
+                    <option value="working">Working</option>
+                </select>
+            </div>
         </div>
     );
 }
