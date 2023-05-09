aa78a0cacc8969f4badac57ecf618351d1d9fa91
MultimedieGunvor <eaagupa@students.eaaa.dk>
2023-05-09 15:11:36 +0200

    Popup switch

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -4,48 +4,55 @@ import ModWeight from "./ModWeight";
 import ModPod from "./ModPod";
 
 const Popup = ({doc, show, onClose, folder}) => {
-    const [option, setOption] = useState("popup");
+    const [option, setOption] = useState('popup'); 
     // console.log("show i popup: ", show);
 
     if(!show) {
         return null;
     }
     console.log("option: ", option);
-
-    function chooseOption(prop) { /* Change to selectOption(prop)? Then pass the option as a prop where the function is called? */
-        setOption(prop);
-    };
+    const handleClick = (optionState) => {
+        setOption(optionState);
+        console.log("option: ", option);
+    }
 
     return (
         <div className="popup">
             <div className="popup-content" onClick={(e) => e.stopPropagation()}>
-                {option === "popup" ? ( 
-                <ul className="popup-options">
-                    <li className="option">Copy cell</li>
-                    <li className="option">Release container</li>
-                    <li className="option">Lock container</li>
-                    <li className="option">Set auto lock</li>
-                    <li className="option">Accept colli and weight differences</li>
-                    <li className="option">Clear accepted differences</li>
-                    <li className="option clickable" onClick={setOption("modWeight")}>Modify colli and B/L weight</li>
-                    <li className="option clickable" onClick={setOption("modPod")}>Modify POD</li>
-                    <li className="option">Edit/add container note</li>
-                    <li className="option">Edit cust ref note</li>
-                    <li className="option">View consignee and consignor</li>
-                    <li className="option">Change status to wait at (5)</li>
-                    <li className="option">Change status to locked (7)</li>
-                    <li className="option">Change status to done (8nd)</li>
-                    <li className="option">Change status to cancel (8nc)</li>
-                    <li className="option">Change status to open (201)</li>
-                    <li className="option">Change status to error (210)</li>
-                    <li className="option">View</li>
-                    <li className="option">Compact container no</li>
-                </ul>
-                ) : option === "modWeight" ? (
-                <ModWeight BL={doc} folder={folder} onClose={onClose}/> //  <-- Fix this! Use switch instead
-                ) : (
-                <ModPod BL={doc} folder={folder} onClose={onClose}/>
-                )}
+                {(() => {
+                    switch (option) {
+                        case 'modWeight':
+                            return <ModWeight BL={doc} folder={folder} onClose={onClose}/>
+                        case 'modPod':
+                            return <ModPod BL={doc} folder={folder} onClose={onClose}/>
+                        case 'popup':
+                            return (
+                                <ul className="popup-options">
+                                    <li className="option">Copy cell</li>
+                                    <li className="option">Release container</li>
+                                    <li className="option">Lock container</li>
+                                    <li className="option">Set auto lock</li>
+                                    <li className="option">Accept colli and weight differences</li>
+                                    <li className="option">Clear accepted differences</li>
+                                    <li className="option clickable" onClick={handleClick('modWeight')}>Modify colli and B/L weight</li>
+                                    <li className="option clickable" onClick={handleClick('modPod')}>Modify POD</li>
+                                    <li className="option">Edit/add container note</li>
+                                    <li className="option">Edit cust ref note</li>
+                                    <li className="option">View consignee and consignor</li>
+                                    <li className="option">Change status to wait at (5)</li>
+                                    <li className="option">Change status to locked (7)</li>
+                                    <li className="option">Change status to done (8nd)</li>
+                                    <li className="option">Change status to cancel (8nc)</li>
+                                    <li className="option">Change status to open (201)</li>
+                                    <li className="option">Change status to error (210)</li>
+                                    <li className="option">View</li>
+                                    <li className="option">Compact container no</li>
+                                </ul>
+                            )
+                        default:
+                            return null
+                    }
+                })()}
             </div>
         </div>
     );
